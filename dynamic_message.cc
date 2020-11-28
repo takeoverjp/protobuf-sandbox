757aa843240aed06d9b63a4aa53661983f9c0afb
Kondo Takeo <takeoverjp@gmail.com>
2020-11-29 02:23:35 +0900

    Add LoadMessageBasedOnMessage.

--- dynamic_message.cc
+++ dynamic_message.cc
@@ -1,5 +1,6 @@
 #include <google/protobuf/dynamic_message.h>
 #include <google/protobuf/generated_message_reflection.h>
+#include <google/protobuf/util/message_differencer.h>
 #include <unistd.h>
 
 #include <fstream>
@@ -8,25 +9,31 @@
 #include "sample.pb.h"
 
 using ::google::protobuf::Descriptor;
+using ::google::protobuf::DynamicCastToGenerated;
 using ::google::protobuf::DynamicMessageFactory;
 using ::google::protobuf::Message;
+using ::google::protobuf::util::MessageDifferencer;
 using ::sandbox::SampleMessage;
 
 const std::string kMessageFile = "message.data";
 
 static void StoreMessage(const std::string& name, const Message& message) {}
 
+static void InitializeSampleMessage(SampleMessage* message) {
+  message->set_string_field("string field");
+  message->set_int_field(2);
+  message->set_bool_field(true);
+  message->add_repeated_int_field(4);
+  message->add_repeated_int_field(5);
+  message->add_repeated_int_field(6);
+  message->mutable_embedded_message_field()->set_string_field(
+      "embedded string field");
+  message->mutable_embedded_message_field()->set_int_field(7);
+}
+
 static void SetUp() {
   sandbox::SampleMessage message;
-  message.set_string_field("string field");
-  message.set_int_field(2);
-  message.set_bool_field(true);
-  message.add_repeated_int_field(4);
-  message.add_repeated_int_field(5);
-  message.add_repeated_int_field(6);
-  message.mutable_embedded_message_field()->set_string_field(
-      "embedded string field");
-  message.mutable_embedded_message_field()->set_int_field(7);
+  InitializeSampleMessage(&message);
 
   {
     std::ofstream output(kMessageFile, std::ios::trunc | std::ios::binary);
@@ -37,6 +44,13 @@ static void SetUp() {
 }
 
 static void TearDown() { unlink(kMessageFile.c_str()); }
+
+static void CheckSampleMessage(const SampleMessage& message) {
+  // std::cout << message.DebugString() << std::endl;
+  SampleMessage expectation;
+  InitializeSampleMessage(&expectation);
+  assert(MessageDifferencer::Equivalent(expectation, message));
+}
 
 static std::unique_ptr<Message> LoadMessageBasedOnDescriptor(
     DynamicMessageFactory* factory, const std::string file_name,
@@ -67,26 +81,67 @@ static std::unique_ptr<Message> LoadMessageBasedOnDescriptor(
   return message;
 }
 
-int main() {
-  SetUp();
-
-  SampleMessage sample;
-
-  {
-    DynamicMessageFactory factory;
-    std::unique_ptr<Message> message = LoadMessageBasedOnDescriptor(
-        &factory, kMessageFile, SampleMessage::descriptor());
-    sample.CopyFrom(*message);
-
-    // 4. ここでDynamicMessageFactory解放
-    //    以降、factoryから生成したmessageは参照してはいけない
+static std::unique_ptr<Message> LoadMessageBasedOnMessage(
+    const std::string file_name, const Message& instance) {
+  // 1. Message::Newで、mutableなMessageを取得
+  //    得たMessageは呼び出し元が解放しなければならないので、unique_ptrで受ける
+  auto message = std::unique_ptr<Message>(instance.New());
+  if (message == nullptr) {
+    return nullptr;
   }
 
-  std::cout << sample.DebugString() << std::endl;
+  // 2. Message::ParseFromXXXで、Messageをdeserialize
+  {
+    std::ifstream input(file_name, std::ios::binary);
+    message->ParseFromIstream(&input);
+  }
 
-  google::protobuf::ShutdownProtobufLibrary();
+  return message;
+}
+
+static void TestLoadMessageBasedOnMessage() {
+  SetUp();
+
+  std::unique_ptr<Message> message = LoadMessageBasedOnMessage(
+        kMessageFile, SampleMessage::default_instance());
+
+  // 4. 具体的なMessage型にキャスト
+  SampleMessage* sample_message = DynamicCastToGenerated<SampleMessage>(message.get());
+  CheckSampleMessage(*sample_message);
 
   TearDown();
+}
+
+static void TestLoadMessageBasedOnDescriptor() {
+  SetUp();
+
+  SampleMessage sample_message;
+
+  {
+    // 0. DynamicMessageFactoryを生成
+    DynamicMessageFactory factory;
+
+    std::unique_ptr<Message> message = LoadMessageBasedOnDescriptor(
+        &factory, kMessageFile, SampleMessage::descriptor());
+
+    // 4. 具体的なMessage型にコピー
+    sample_message.CopyFrom(*message);
+
+    // 5. ここでDynamicMessageFactory解放
+    //    以降、factoryから生成したmessageは参照してはいけない
+    //    sample_messageは引き続き使用できる
+  }
+
+  CheckSampleMessage(sample_message);
+
+  TearDown();
+}
+
+int main() {
+  TestLoadMessageBasedOnMessage();
+  TestLoadMessageBasedOnDescriptor();
+
+  google::protobuf::ShutdownProtobufLibrary();
 
   return 0;
 }
